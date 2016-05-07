320319f26d8a0453724ae4e367d042fb75a7c020
albertgcsula <albertgcsula@gmail.com>
2016-05-06 21:20:07 -0700

    add partial templates for team and home. modify app js and controllers.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@
 
 angular.module('cloudyHeadsApp', [
   'ngRoute',
-  'cloudyHeadsApp.about'
+  'cloudyHeadsApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/home.html',controller: 'HomeCtrl'});
+  $routeProvider.when('/product', {templateUrl: 'partials/product.html',controller: 'ProductCtrl'});
+  $routeProvider.when('/team', {templateUrl: 'partials/team.html',controller: 'TeamCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
---
+++ js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var cloudyHeadsApp = angular.module('cloudyHeadsApp.controllers', []);
+
+cloudyHeadsApp.controller('HomeCtrl', function ($scope) {
+  console.log('Home Page Controller');
+});
+
+cloudyHeadsApp.controller('ProductCtrl', function ($scope) {
+  console.log('Product Page Controller');
+});
+
+cloudyHeadsApp.controller('TeamCtrl', function ($scope) {
+  console.log('Team Page Controller');
+});
