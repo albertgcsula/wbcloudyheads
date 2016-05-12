a1cffd9710f15e6017d1d07b1770e0f614340cfe
albertgcsula <albertgcsula@gmail.com>
2016-05-11 21:56:25 -0700

    update images. update shop controller, add cart conteoller. update json and routes.

--- js/app.js
+++ js/app.js
@@ -7,9 +7,10 @@ angular.module('cloudyHeadsApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
   $routeProvider.when('/product', {templateUrl: 'partials/product.html', controller: 'ProductCtrl'});
-  $routeProvider.when('/product/:productId', {templateUrl: 'partials/shop/product-detail.html', controller: 'ProductDetailCtrl'});
+  $routeProvider.when('/product/:productType/:productId', {templateUrl: 'partials/shop/product-detail.html', controller: 'ProductDetailCtrl'});
   $routeProvider.when('/team', {templateUrl: 'partials/team.html', controller: 'TeamCtrl'});
   $routeProvider.when('/shop/printed', {templateUrl: 'partials/shop/printed.html', controller: 'ShopCtrl'});
   $routeProvider.when('/shop/printable', {templateUrl: 'partials/shop/printable.html', controller: 'ShopCtrl'});
+  $routeProvider.when('/cart', {templateUrl: 'partials/shop/cart.html', controller: 'CartCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,8 @@
 
 var cloudyHeadsApp = angular.module('cloudyHeadsApp.controllers', []);
 
+var cartItems = [];
+
 cloudyHeadsApp.controller('HomeCtrl', function ($scope) {
   $('body').addClass('home');
 });
@@ -13,29 +15,65 @@ cloudyHeadsApp.controller('ProductCtrl', function ($scope) {
 cloudyHeadsApp.controller('ProductDetailCtrl', function ($scope, $routeParams, $http) {
   $('body').addClass('product-detail');
   $scope.productId = $routeParams.productId;
+  $scope.productType = $routeParams.productType;
 
-  var jsonUrl = getJsonUrl();
+  var jsonUrl = "json/" + $scope.productType + "-products.json";
   $http.get(jsonUrl).then(function(response) {
     $scope.product = response.data.products[$scope.productId];
+  });
+
+  // add to cart functionality
+  var $cartStatus = $('#cartStatus');
+  var $cartNav = $('#cartNav');
+  var cartItem = {};
+  $('#addCart').on('click', function() {
+    $cartStatus.html('<p>Success! Your item has been added to cart!</p>');
+    if ($('#customText').val() !== '') {
+      $cartStatus.append('<p>Custom text added: ' + $('#customText').val() +'</p>');
+    }
+
+    if ($('input[name=optionsRadios]:checked').val() !== undefined) {
+      $cartStatus.append('<p>Custom option added: ' + $('input[name=optionsRadios]:checked').val() +'</p>');
+    }
+
+    cartItem.id = $('#productId').val();
+    cartItem.title = $('#productTitle').val();
+    cartItem.description = $('#productDescription').val();
+    cartItem.imageUrl = $('#productImage').val();
+
+    cartItems.push(cartItem);
+
+    $cartStatus.show();
+    $cartNav.show();
   });
 });
 
 cloudyHeadsApp.controller('TeamCtrl', function ($scope) {
-  console.log('Team Page Controller');
+  $('body').addClass('team');
 });
 
 cloudyHeadsApp.controller('ShopCtrl', function ($scope, $http) {
   $('body').addClass('shop');
+
   var jsonUrl = getJsonUrl();
   $http.get(jsonUrl).then(function(response) {
     $scope.products = response.data.products;
   });
 });
 
+cloudyHeadsApp.controller('CartCtrl', function ($scope) {
+  $('body').addClass('cart');
+  $scope.cartItems = cartItems;
+
+  if ($scope.cartItems.length > 0) {
+    $('.checkout-section').show();
+  }
+});
+
 var getJsonUrl = function () {
   var jsonUrl = "json/";
 
-  if(window.location.href .indexOf('printable') > -1) {
+  if(window.location.href.indexOf('printable') > -1) {
     jsonUrl = "json/printable-products.json";
   } else {
     jsonUrl = "json/printed-products.json";
