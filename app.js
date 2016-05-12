8f752f7e00a9905257a2d829e173c8a66c850760
dcintron <davecintron@outlook.com>
2016-05-12 10:57:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('cloudyHeadsApp', [
+  'ngRoute',
+  'cloudyHeadsApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/customize', { templateUrl: 'partials/product-customize.html', controller: 'ProductCtrl' });
+  $routeProvider.when('/product', { templateUrl: 'partials/product.html', controller: 'ProductCtrl' });
+  $routeProvider.when('/product/:productType/:productId', {templateUrl: 'partials/shop/product-detail.html', controller: 'ProductDetailCtrl'});
+  $routeProvider.when('/team', {templateUrl: 'partials/team.html', controller: 'TeamCtrl'});
+  $routeProvider.when('/shop/printed', {templateUrl: 'partials/shop/printed.html', controller: 'ShopCtrl'});
+  $routeProvider.when('/shop/printable', {templateUrl: 'partials/shop/printable.html', controller: 'ShopCtrl'});
+  $routeProvider.when('/cart', {templateUrl: 'partials/shop/cart.html', controller: 'CartCtrl'});
+  $routeProvider.when('/nfc/:nfcId', {templateUrl: 'partials/shop/cart.html', controller: 'NfcCtrl'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
